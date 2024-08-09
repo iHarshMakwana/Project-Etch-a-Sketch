5e82150e6dc7f7406b6fdba28423b96bedd52cad
iHarshMakwana <[142649044+iHarshMakwana@users.noreply.github.com]>
2024-08-09 18:36:41 +0530

    Final Commit

--- main.js
+++ main.js
@@ -8,9 +8,66 @@ function createBoard(size){
 
         for(let i = 0;i < numDivs;i++){
             let div = document.createElement("div");
-            div.style.backgroundColor= "yellow";
+            div.addEventListener("mouseover",colordiv);
             board.insertAdjacentElement("beforeend",div);
         }
 }
  
-createBoard(10);
+function getsize(){
+    let input = prompt("What will be The Size?")
+    let message = document.querySelector("#message");
+
+    if(input == ""){
+        message.innerHTML="Please Enter Valid Number";
+    }else if(input < 0 || input > 100){
+        message.innerHTML="Enter Number Between 0 and 100";
+    }else{
+        message.innerHTML="Now You can play!";
+        return input;
+    }
+}
+
+function colordiv(){
+    if(click){
+        if(color == "random"){
+            this.style.backgroundColor =   `hsl(${Math.random()*360},100%,50%)`
+        }else{
+            this.style.backgroundColor = 'black'
+        }
+    }
+}
+
+function setcolor(colorchoice){
+    color = colorchoice;
+}
+
+let color = "black";
+
+let btnpopup = document.querySelector("#popup");
+btnpopup.addEventListener("click",function(){
+    let size = getsize();
+    createBoard(size);
+
+})
+
+
+function resetboard(){
+    let divs =document.querySelectorAll("div");
+    divs.forEach((div) => div.style.backgroundColor = "white");
+    };
+
+let click = false;
+
+document.querySelector("body").addEventListener("click",function(e){
+    if(e.target.tagName != "BUTTON")
+    {
+        click = !click;
+        let draw = document.querySelector("draw");
+        if(click){
+            draw.innerHTML = "Now you can Draw";
+        }
+        else{
+            draw.innerHTML = "You cant Draw";
+        }
+    }
+})
